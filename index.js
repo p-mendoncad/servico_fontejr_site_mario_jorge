22f3019f2ef9d74e12bef282dfc35b7643551189
p-mendoncad <125631065+p-mendoncad@users.noreply.github.com>
2024-05-21 00:05:43 -0300

    ajustes css para o footer

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-
-function scroll_inicio(){
-    window.scrollTo(0,0);
- }
-
 function scroll_comercial(){
-    //window.scrollTo(0,y);
+    window.scrollTo(0,0);
  }
 
 function scroll_residencial(){
