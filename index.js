a194b29c0bb88d6a5fe6cd71927d709c043d2ba6
p-mendoncad <125631065+p-mendoncad@users.noreply.github.com>
2024-05-20 23:44:30 -0300

    scrollScript, header fixo, alteracoes menu e ajustes css

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function scroll_inicio(){
+    window.scrollTo(0,0);
+ }
+
+function scroll_comercial(){
+    //window.scrollTo(0,y);
+ }
+
+function scroll_residencial(){
+     //window.scrollTo(0,y);
+ }
+
+ function scroll_sobre(){
+     //window.scrollTo(0,y);
+ }
